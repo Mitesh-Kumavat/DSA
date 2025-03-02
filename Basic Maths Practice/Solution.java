74f83d47906e926a06d305856040a012f00de47c
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-03-02 21:08:36 +0530

    first commit

---
+++ Basic Maths Practice/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * Leetcode:https://leetcode.com/problems/palindrome-number/
+ * 
+ * -> Test case:
+ * Input: 121
+ * Output: true
+ * 
+ * Input: -121
+ * Output: false
+ * 
+ * Input: 10
+ * Output: false
+ */
+public class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        long rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (temp == (int) rev && temp >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isPalindrome(000));
+    }
+}
---
+++ Basic Maths Practice/reverseNumber.java
@@ -0,0 +1,37 @@
+/*
+ * Problem Statement: Given a 32-bit signed integer, reverse digits of an integer.
+ * Leetcode: https://leetcode.com/problems/reverse-integer/
+ * 
+ * -> Test case:
+ * Input: 123
+ * Output: 321
+ * 
+ * Input: -123
+ * Output: -321
+ * 
+ * Input: 120
+ * Output: 21
+ * 
+ * Input: 1534236469
+ * Output: 0
+ */
+class Solution {
+    public int reverse(int x) {
+        int temp = x;
+        long rev = 0;
+
+        while (temp != 0) {
+            rev = 10 * rev + temp % 10;
+            temp = temp / 10;
+        }
+        if (rev == (int) rev) // this condition makes sure there has been no overflow
+            return (int) rev; // print output in required data type
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.reverse(-12300));
+    }
+}
